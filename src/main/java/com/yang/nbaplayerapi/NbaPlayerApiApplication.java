37d8124ddb6c28f24204826566d97a83d6298b48
jyang001 <joeyang095@gmail.com>
2020-02-24 01:22:24 -0500

    Initial commit

---
+++ src/main/java/com/yang/nbaplayerapi/NbaPlayerApiApplication.java
@@ -0,0 +1,13 @@
+package com.yang.nbaplayerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaPlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaPlayerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yang/nbaplayerapi/NbaPlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.nbaplayerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaPlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
