f23d405ea527ceb9d28bf09da7dfababa432a348
jyang001 <joeyang095@gmail.com>
2020-07-06 01:23:45 -0400

    add appropriate Lombok annotations to PlayerInfo class

--- src/main/java/com/yang/nbaplayerapi/model/PlayerInfo.java
+++ src/main/java/com/yang/nbaplayerapi/model/PlayerInfo.java
@@ -1,9 +1,15 @@
 package com.yang.nbaplayerapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties
 public class PlayerInfo {
 
     @JsonProperty("firstName")
