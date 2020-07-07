b248c7609d0f5235b2f05525d0e9552380497a29
jyang001 <joeyang095@gmail.com>
2020-07-07 00:29:54 -0400

    add RestTemplate bean

--- src/main/java/com/yang/nbaplayerapi/NbaPlayerApiApplication.java
+++ src/main/java/com/yang/nbaplayerapi/NbaPlayerApiApplication.java
@@ -2,9 +2,17 @@ package com.yang.nbaplayerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NbaPlayerApiApplication {
+
+	@Bean
+	public RestTemplate getRestTempalte() {
+		return new RestTemplate();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(NbaPlayerApiApplication.class, args);
--- src/main/java/com/yang/nbaplayerapi/controller/PlayerController.java
+++ src/main/java/com/yang/nbaplayerapi/controller/PlayerController.java
@@ -3,11 +3,13 @@ package com.yang.nbaplayerapi.controller;
 import com.yang.nbaplayerapi.model.PlayerInfo;
 import com.yang.nbaplayerapi.utils.PlayerUtil;
 import com.yang.nbaplayerapi.wrapper.PlayerCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -16,6 +18,9 @@ import java.util.Set;
 
 @RestController
 public class PlayerController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/api/ping")
     public ResponseEntity<String> pingTest() {
