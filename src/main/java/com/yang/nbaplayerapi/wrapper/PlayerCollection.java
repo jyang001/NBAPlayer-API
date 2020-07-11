e352b7b5e0eb00578295ec5a30e6ba8587975245
jyang001 <joeyang095@gmail.com>
2020-07-11 02:04:20 -0400

    add JsonDeserialize annotation to PlayerCollection class

--- src/main/java/com/yang/nbaplayerapi/wrapper/PlayerCollection.java
+++ src/main/java/com/yang/nbaplayerapi/wrapper/PlayerCollection.java
@@ -1,11 +1,14 @@
 package com.yang.nbaplayerapi.wrapper;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.yang.nbaplayerapi.model.PlayerInfo;
+import com.yang.nbaplayerapi.utils.PlayerCollectionJsonDeserializer;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@JsonDeserialize( using = PlayerCollectionJsonDeserializer.class)
 public class PlayerCollection {
 
     private Set<PlayerInfo> playerInfo;
