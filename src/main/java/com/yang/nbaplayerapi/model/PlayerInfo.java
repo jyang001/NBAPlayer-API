4ee9b3225de3bec01b9f2a3bbc13db9033bd47bb
jyang001 <joeyang095@gmail.com>
2020-07-18 22:36:23 -0400

    Add heightCentimeters attribute to PlayerInfo class

--- src/main/java/com/yang/nbaplayerapi/model/PlayerInfo.java
+++ src/main/java/com/yang/nbaplayerapi/model/PlayerInfo.java
@@ -2,9 +2,7 @@ package com.yang.nbaplayerapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
@@ -31,9 +29,28 @@ public class PlayerInfo {
     @JsonProperty("heightFeet")
     String heightFeet;
 
+    @JsonProperty("heightMeters")
+    @Setter(AccessLevel.NONE)
+    String heightMeters;
+
+    String heightCentimeters;
+
     @JsonProperty("heightInches")
     String heightInches;
 
     @JsonProperty("yearsPro")
     String yearsPro;
+
+    public void setHeightMeters(String heightMeters) {
+        if (heightMeters.equals("")) {
+            this.heightMeters = "-";
+            this.heightCentimeters = "-";
+        }
+        else {
+            this.heightMeters = heightMeters;
+            double tempHeightMeters = Double.parseDouble(heightMeters);
+            setHeightCentimeters(Double.toString(tempHeightMeters * 100));
+        }
+    }
+
 }
