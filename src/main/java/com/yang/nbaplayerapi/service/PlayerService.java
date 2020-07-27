81dd20a7effeb63ae36a7753c451ec3c377ad91c
jyang001 <joeyang095@gmail.com>
2020-07-26 23:48:07 -0400

    implement getOrderedPlayers()

--- src/main/java/com/yang/nbaplayerapi/service/PlayerService.java
+++ src/main/java/com/yang/nbaplayerapi/service/PlayerService.java
@@ -1,9 +1,14 @@
 package com.yang.nbaplayerapi.service;
 
+import com.yang.nbaplayerapi.comparator.FirstNameComparator;
+import com.yang.nbaplayerapi.model.PlayerInfo;
 import com.yang.nbaplayerapi.wrapper.PlayerCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class PlayerService {
@@ -11,8 +16,23 @@ public class PlayerService {
     @Autowired
     RestTemplate restTemplate;
 
+    private PlayerCollection playerCollection;
+
     public PlayerCollection getAllPlayers() {
         return restTemplate.getForObject("http://data.nba.net/prod/v1/2019/players.json", PlayerCollection.class);
+    }
+
+    public PlayerCollection getOrderedPlayers(String orderBy, String direction) {
+        playerCollection = getAllPlayers();
+        List<PlayerInfo> playerList = playerCollection.getPlayerInfo();
+
+        switch(orderBy) {
+            case "firstname":
+                Collections.sort(playerList, new FirstNameComparator());
+            default:
+        }
+        playerCollection.setPlayerInfo(playerList);
+        return playerCollection;
     }
 
 }
